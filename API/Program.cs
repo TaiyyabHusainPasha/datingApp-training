d75a2db7c857c1880f44499f2628c0869780e10f
taiyyab-husain-taiyyab-husain <taiyyab-husain.taiyyab-husain@capgemini.com>
2026-04-18 07:37:28 +0530

    session one two three

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(ops =>
+{
+    ops.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("https://localhost:4200","http://localhost:4200"));
+
+app.MapControllers();
+
+app.Run();
